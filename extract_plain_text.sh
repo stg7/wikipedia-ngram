923c47f76c47f13e617b340bc85d84c460457ac9
Steve <stg7@gmx.de>
2016-08-14 23:39:13 +0200

    add more files for first processing steps

---
+++ extract_plain_text.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python2 ./tools/wikipedia-extractor/WikiExtractor.py -c -o out dataset/enwiki-*-pages-articles-multistream.xml.bz2
---
+++ prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p tools
+cd tools
+git clone "https://github.com/bwbaugh/wikipedia-extractor.git"
+cd ..
