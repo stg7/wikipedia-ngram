d17a748f4a3ce0c4a61f75c4fc6259b9373253d9
Steve <stg7@gmx.de>
2016-08-15 00:33:10 +0200

    change to py3

--- extract_plain_text.sh
+++ extract_plain_text.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python2 ./tools/wikiextractor/WikiExtractor.py -c -o out dataset/enwiki-*-pages-articles-multistream.xml.bz2
+python3 ./tools/wikiextractor/WikiExtractor.py -c -o out dataset/enwiki-*-pages-articles-multistream.xml.bz2
